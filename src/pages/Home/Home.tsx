88a6aecaa29258fa87ff9b31f4c402d72808330e
WONWOOL <wonwoo@goodganglabs.com>
2023-03-06 23:14:37 +0900

    feat-WW: prevent touch

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
     <TouchScrollProvider>
       <div
         ref={scrollRef}
-        className={`bg-[#1A1A1A] overflow-y-scroll ${styles.smoothScroll}`}
+        className={`bg-[#1A1A1A] overflow-y-scroll touch-none ${styles.smoothScroll}`}
         style={{ height: "calc(var(--vh, 1vh) * 100)" }}
       >
         <MainIntro />
